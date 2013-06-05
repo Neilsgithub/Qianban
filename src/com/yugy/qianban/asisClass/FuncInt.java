51a350184b3f8dc4d6bd7eb1d43ce6c73f72d70d
kyze8439690 <me@yanghui.name>
2013-06-05 22:43:53 +0800

    add FuncInt

---
+++ src/com/yugy/qianban/asisClass/FuncInt.java
@@ -0,0 +1,18 @@
+package com.yugy.qianban.asisClass;
+
+import android.content.Context;
+
+public class FuncInt {
+
+	public static int dp(Context context, float dp){
+		final float scale = context.getResources().getDisplayMetrics().density;  
+        return (int) (dp * scale + 0.5f);
+	}
+	
+	public static int getScreenWidth(Context context){
+		
+		return context.getResources().getDisplayMetrics().widthPixels;
+		
+	}
+	
+}

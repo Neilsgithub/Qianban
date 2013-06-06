0df1861c695c78f8eb3d7e7a6ade54ede7728121
kyze8439690 <me@yanghui.name>
2013-06-06 13:42:47 +0800

    add Conf and Func class

---
+++ src/com/yugy/qianban/asisClass/Conf.java
@@ -0,0 +1,8 @@
+package com.yugy.qianban.asisClass;
+
+public class Conf {
+	
+	public static boolean debug = true;
+	public static String TAG = "Qianban";
+	
+}
---
+++ src/com/yugy/qianban/asisClass/Func.java
@@ -0,0 +1,11 @@
+package com.yugy.qianban.asisClass;
+
+import android.util.Log;
+
+public class Func {
+
+	public static void log(String log){
+		Log.d(Conf.TAG, log);
+	}
+	
+}

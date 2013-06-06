bf12a55566f52111acd048fc1cfcdd7cc4a5dfa7
kyze8439690 <me@yanghui.name>
2013-06-06 09:35:35 +0800

    fix bug

--- src/com/yugy/qianban/asisClass/FuncInt.java
+++ src/com/yugy/qianban/asisClass/FuncInt.java
@@ -1,7 +1,6 @@
 package com.yugy.qianban.asisClass;
 
 import android.content.Context;
-<<<<<<< HEAD
 
 public class FuncInt {
 
@@ -15,24 +14,4 @@ public class FuncInt {
 		return context.getResources().getDisplayMetrics().widthPixels;
 		
 	}
-	
-=======
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
-
-public class FuncInt {
-	
-	public static int dp(Context context, double d) {  
-        final float scale = context.getResources().getDisplayMetrics().density;  
-        return (int) (d * scale + 0.5f);
-
-	}
-	
-	public static int getScreenWidth(Context context){
-		DisplayMetrics dm = new DisplayMetrics();
-        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        windowManager.getDefaultDisplay().getMetrics(dm);
-        return dm.widthPixels;
-	}
->>>>>>> 04c47ac922fd4430cb0a2c647602c4494d62a84a
 }

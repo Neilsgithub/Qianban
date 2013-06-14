244e8a480497f8960662231aa526e624171310b2
kyze8439690 <me@yanghui.name>
2013-06-14 15:04:05 +0800

    add animation add lrc download

--- src/com/yugy/qianban/sdk/GeCiMe.java
+++ src/com/yugy/qianban/sdk/GeCiMe.java
@@ -1,6 +1,7 @@
 package com.yugy.qianban.sdk;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class GeCiMe {
@@ -13,10 +14,14 @@ public class GeCiMe {
 		}
 	}
 	
-	public static void getLrc(String songName, String authorName, JsonHttpResponseHandler responseHandler){
+	public static void searchLrc(String songName, String authorName, JsonHttpResponseHandler responseHandler){
 		init();
 		client.get("http://geci.me/api/lyric/" + songName + "/" + authorName, responseHandler);
 	}
 	
+	public static void downloadLrc(String lrcUrl, AsyncHttpResponseHandler responseHandler){
+		init();
+		client.get(lrcUrl, responseHandler);
+	}
 	
 }
